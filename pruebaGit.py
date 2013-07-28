545cd2268027d1fcf6dbdec0ce97c721572667df
Jorge Vargas <jorge.vargas@gmail.com>
2013-07-27 20:58:44 -0400

    pythonic newlines
    print sin parametros imprime un newline x default y se puede usar *
    para imprimir varios newlines

--- pruebaGit.py
+++ pruebaGit.py
@@ -16,7 +16,7 @@ print chr(27) + "[0;36m" + "__  __ \_  /_  __ `/  ___/_  //_/  __ \__  __ \_  __
 print chr(27) + "[0;36m" + "_  /_/ /  / / /_/ // /__ _  ,<  / /_/ /_  /_/ /(__  ) "
 print chr(27) + "[0;36m" + "/_.___//_/  \__,_/ \___/ /_/|_| \____/_  .___//____/  "
 print chr(27) + "[0;36m" + "                                    /_/              "
-print "\n"
+print
 
 
 def ver_contenido():
@@ -30,14 +30,15 @@ def ver_contenido():
         # con esto decimos que si un elemento n de el objeto e es igual a
         # lo que entra por stdin se ejecuten las siguientes declaraciones
         if i == variable:
-            print "\n"
-            print "el contenido de su variable es:\n %s" % e[variable]
-            print "\n\n\n\n\n"
+            print 
+            print "el contenido de su variable es: %s" % e[variable]
+            print 5*"\n"
 
 
 def main():
-    print "menu\n"
-    print "1)ver el contenido de la variable"
+    print "menu"
+    print
+    print "1) ver el contenido de la variable"
     opcion = raw_input(">")
     if opcion == "1":
         ver_contenido()
